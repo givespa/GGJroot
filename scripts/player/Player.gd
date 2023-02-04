a4d8c44146ba4a8bf3ec59c49b44311a33c64f11
Gian vespa <givespas@gmail.com>
2023-02-04 17:17:08 -0500

    fix: clear line code

--- scripts/player/Player.gd
+++ scripts/player/Player.gd
@@ -10,21 +10,23 @@ func move_controllers():
 		velocity.x = SPEED 
 		$AnimatedSprite.flip_h = false
 		$AnimatedSprite.play("run")
-
+	
 	if Input.is_action_pressed("left"):
 		velocity.x = -SPEED
 		$AnimatedSprite.flip_h = true
 		$AnimatedSprite.play("run")
+	
 	if Input.is_action_pressed("up"):
 		velocity.y = -SPEED
 		$AnimatedSprite.play("run")
+
 	if Input.is_action_pressed("down"):
 		velocity.y = SPEED
 		$AnimatedSprite.play("run")
 
 	if !Input.is_action_pressed("right") and !Input.is_action_pressed("left") and !Input.is_action_pressed("up") and !Input.is_action_pressed("down"):
 		$AnimatedSprite.play("idle")
-		
+	
 	velocity = move_and_slide(velocity, Vector2.UP)
 	velocity.x = lerp(velocity.x, 0, 0.1)
 	velocity.y = lerp(velocity.y, 0, 0.1)
