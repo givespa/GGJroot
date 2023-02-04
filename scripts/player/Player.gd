d2a0baa76beea1abff16419de968cf603a20d0ce
Gian vespa <givespas@gmail.com>
2023-02-04 17:10:52 -0500

    fix: refactor code (controller player)

--- scripts/player/Player.gd
+++ scripts/player/Player.gd
@@ -4,36 +4,32 @@ var velocity = Vector2(0,0)
 const SPEED = 150
 export var life = 3
 
-func _physics_process(delta):
+func move_controllers():
+	
 	if Input.is_action_pressed("right"):
-		print("RIGHT")
 		velocity.x = SPEED 
 		$AnimatedSprite.flip_h = false
 		$AnimatedSprite.play("run")
-		#if sign($Position2D.position.x) == -1:
-			#$Position2D.position.x *= -1
-			
-	elif Input.is_action_pressed("left"):
-		print("LEFT")
+
+	if Input.is_action_pressed("left"):
 		velocity.x = -SPEED
 		$AnimatedSprite.flip_h = true
 		$AnimatedSprite.play("run")
-			
-	elif Input.is_action_pressed("up"):
-		print("UP")
+	if Input.is_action_pressed("up"):
 		velocity.y = -SPEED
 		$AnimatedSprite.play("run")
-			
-	elif Input.is_action_pressed("down"):
-		print("DOWN")
+	if Input.is_action_pressed("down"):
 		velocity.y = SPEED
 		$AnimatedSprite.play("run")
-	else:
+
+	if !Input.is_action_pressed("right") and !Input.is_action_pressed("left") and !Input.is_action_pressed("up") and !Input.is_action_pressed("down"):
 		$AnimatedSprite.play("idle")
 		
 	velocity = move_and_slide(velocity, Vector2.UP)
 	velocity.x = lerp(velocity.x, 0, 0.1)
 	velocity.y = lerp(velocity.y, 0, 0.1)
+	
+#	// ------------------ function end -----------------
 
-
-
+func _physics_process(delta):
+	move_controllers()
