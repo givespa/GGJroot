98bfcc06f8c89167669ecd4cb57e941c0c04e649
Gian vespa <givespas@gmail.com>
2023-02-04 17:26:31 -0500

    reactor: export var + data type

--- scripts/player/Player.gd
+++ scripts/player/Player.gd
@@ -1,35 +1,35 @@
 extends KinematicBody2D
 
-var velocity = Vector2(0,0)
-const SPEED = 150
-export var life = 3
+var _position = Vector2(0,0)
+export(int) var speed = 150
+export(int) var life = 3
 
 func move_controllers():
 	
 	if Input.is_action_pressed("right"):
-		velocity.x = SPEED 
+		_position.x = speed
 		$AnimatedSprite.flip_h = false
 		$AnimatedSprite.play("run")
 	
 	if Input.is_action_pressed("left"):
-		velocity.x = -SPEED
+		_position.x = -speed
 		$AnimatedSprite.flip_h = true
 		$AnimatedSprite.play("run")
 	
 	if Input.is_action_pressed("up"):
-		velocity.y = -SPEED
+		_position.y = -speed
 		$AnimatedSprite.play("run")
 
 	if Input.is_action_pressed("down"):
-		velocity.y = SPEED
+		_position.y = speed
 		$AnimatedSprite.play("run")
 
 	if !Input.is_action_pressed("right") and !Input.is_action_pressed("left") and !Input.is_action_pressed("up") and !Input.is_action_pressed("down"):
 		$AnimatedSprite.play("idle")
 	
-	velocity = move_and_slide(velocity, Vector2.UP)
-	velocity.x = lerp(velocity.x, 0, 0.1)
-	velocity.y = lerp(velocity.y, 0, 0.1)
+	_position = move_and_slide(_position, Vector2.UP)
+	_position.x = lerp(_position.x, 0, 0.1)
+	_position.y = lerp(_position.y, 0, 0.1)
 	
 #	// ------------------ function end -----------------
 
