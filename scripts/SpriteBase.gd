7f4fc2f753d6b5a9e0eaca3458f08ff44ca37d7d
Gian vespa <givespas@gmail.com>
2023-02-05 12:10:33 -0500

    feat: script global + script sprite base

---
+++ scripts/SpriteBase.gd
@@ -0,0 +1,13 @@
+class_name SpriteBase extends KinematicBody2D
+
+var _position = Vector2.ZERO
+enum Direction {RIGHT, LEFT}
+export(Direction) var direction
+export(String) var animatedSprite = "idle"
+export(bool) var activatedController
+export(int) var speed = 150
+export(int) var life = 3
+
+func _ready():
+	$AnimatedSprite.play(self.animatedSprite)
+	$AnimatedSprite.flip_h = true if direction == Direction.RIGHT else false
